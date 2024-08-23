a5d2d074705c52cc18ca1e5f8ca7733f1ed10d1d
invictus <invictusnitude@gmail.com>
2024-08-23 17:56:47 +0300

    pool config change

--- lib/pooly.ex
+++ lib/pooly.ex
@@ -1,10 +1,15 @@
 defmodule Pooly do
   use Application
-  
+
   alias Pooly.Server
 
   def start(_type, _args) do
-    pool_config = [mfa: {Pooly.SampleWorker, :start_link, [[]]}, size: 5]
+    pool_config = [
+      [name: "Pool1", mfa: {Pooly.SampleWorker, :start_link, [[]]}, size: 5],
+      [name: "Pool2", mfa: {Pooly.SampleWorker, :start_link, [[]]}, size: 3],
+      [name: "Pool3", mfa: {Pooly.SampleWorker, :start_link, [[]]}, size: 4]
+    ]
+
     Pooly.Supervisor.start_link(pool_config)
   end
 
